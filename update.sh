296f0c3798fc5ce31dde4fc5ba0284f7e6c0a9b3
AJ Moon <alex.jared.moon@gmail.com>
2017-08-01 22:37:41 -0700

    remove GNU License

--- update.sh
+++ update.sh
@@ -2,22 +2,6 @@
 
 #This script will pull the latest code and proper dependancies for opensource xen-orchestra.
 
-#    Copyright (C) 2017 AJ Moon
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-
 #see the xo project at https://github.com/vatesfr/
 
 source resources/common.sh
